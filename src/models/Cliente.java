1d32e49e18f1b10bab866e3d073ae527ade7ae37
Eduardo Kaique Alberico <109116458+EduardoAlberico@users.noreply.github.com>
2025-06-09 21:28:20 -0300

    add: classe cliente

--- src/models/Cliente.java
+++ src/models/Cliente.java
@@ -1,4 +1,49 @@
 package models;
 
 public class Cliente {
+    private String nome;
+    private String sobrenome;
+    private String RG;
+    private String CPF;
+    private String enderoco;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getRG() {
+        return RG;
+    }
+
+    public void setRG(String RG) {
+        this.RG = RG;
+    }
+
+    public String getCPF() {
+        return CPF;
+    }
+
+    public void setCPF(String CPF) {
+        this.CPF = CPF;
+    }
+
+    public String getEnderoco() {
+        return enderoco;
+    }
+
+    public void setEnderoco(String enderoco) {
+        this.enderoco = enderoco;
+    }
 }
