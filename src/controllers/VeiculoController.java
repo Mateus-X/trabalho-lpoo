1341615d93052d7f2e40b5f8b0c06fbc6313b790
jtakaa <jchemin2k@gmail.com>
2025-06-17 21:41:29 -0300

    <feat: controller cliente e veiculo com base na DAO>

--- src/controllers/VeiculoController.java
+++ src/controllers/VeiculoController.java
@@ -1,7 +1,10 @@
 package controllers;
 
 import dao.RepositorioMemoria;
+import java.util.List;
 import models.Veiculo;
+import models.enums.Categoria;
+import models.enums.Marca;
 import requests.VeiculoRequest;
 
 public class VeiculoController {
@@ -18,5 +21,33 @@ public class VeiculoController {
         } catch (IllegalArgumentException e) {
             System.out.println("Erro ao cadastrar veículo: " + e.getMessage());
         }
+    }
+
+    public List<Veiculo> listarTodosVeiculos() {
+        return repositorioMemoria.listarTodosVeiculos();
+    }
+
+    public Veiculo buscarVeiculoPorPlaca(String placa) {
+        return repositorioMemoria.buscarVeiculoPorPlaca(placa);
+    }
+
+    public List<Veiculo> listarVeiculosDisponiveis() {
+        return repositorioMemoria.listarVeiculosDisponiveis();
+    }
+
+    public List<Veiculo> listarVeiculosLocados() {
+        return repositorioMemoria.listarVeiculosLocados();
+    }
+
+    public List<Veiculo> listarVeiculosParaVenda() {
+        return repositorioMemoria.listarVeiculosParaVenda();
+    }
+
+    public List<Veiculo> filtrarVeiculos(String tipo, Marca marca, Categoria categoria) {
+        return repositorioMemoria.filtrarVeiculos(tipo, marca, categoria);
+    }
+
+    public boolean removerVeiculo(String placa) {
+        return repositorioMemoria.removerVeiculo(placa);
     }
 }
